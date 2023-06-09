0944782ef0b1725a5ed04df251ef6377c66df731
Pratham <ppratham812@gmail.com>
2023-06-09 22:07:55 +0530

    multer setup done and tested

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const multer = require('multer');
 const path = require('path');
 
 // app initializatinon
 const app = express();
+const upload = multer({ dest: "uploads" });
 app.set('view engine', 'ejs');
 
 const PORT = 3000;
@@ -14,4 +16,8 @@ app.listen(PORT, () => {
 app.get('/', (req, res) => {
     const indexFile = path.join(__dirname, '/views/index.ejs');
     res.render(indexFile);
+})
+
+app.post('/upload', upload.single('file'), (req, res) => {
+    res.send('Hii');
 })
