0c81c5bc3c967a5e0dc1eb1fc15fe8a4bc2f1bc3
Pratham <ppratham812@gmail.com>
2023-06-09 21:01:30 +0530

    EJS set as view engine

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
-
+const path = require('path');
 
 // app initializatinon
 const app = express();
+app.set('view engine', 'ejs');
+
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
@@ -10,5 +12,6 @@ app.listen(PORT, () => {
 
 // routes
 app.get('/', (req, res) => {
-    res.send('File Sharing app');
+    const indexFile = path.join(__dirname, '/views/index.ejs');
+    res.render(indexFile);
 })
